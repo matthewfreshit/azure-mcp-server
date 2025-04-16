c90d385f17152f61c3617302459a0d327773a210
Josh Wagner <jwagner5405@gmail.com>
2025-04-15 22:24:08 -0600

    feat: added VirtualMachineTool

---
+++ src/tools/VirtualMachine/DeleteVirtualMachineTool.ts
@@ -0,0 +1,35 @@
+import { z } from 'zod';
+import { BaseAzureVirtualMachineTool, BaseVirtualMachineInput } from './BaseAzureVirtualMachineTool';
+
+type DeleteVMInput = Required<BaseVirtualMachineInput>;
+
+class DeleteVirtualMachineTool extends BaseAzureVirtualMachineTool<DeleteVMInput> {
+    name = 'azure_vms_delete';
+    description = 'Delete an Azure Virtual Machine';
+
+    schema = {
+        subscriptionId: {
+            type: z.string(),
+            description: 'Azure Subscription ID'
+        },
+        resourceGroupName: {
+            type: z.string(),
+            description: 'Azure Resource Group name'
+        },
+        vmName: {
+            type: z.string(),
+            description: 'Virtual Machine name'
+        }
+    };
+
+    async execute(input: DeleteVMInput) {
+        this.validateResourceGroup(input.resourceGroupName);
+        this.validateVmName(input.vmName);
+
+        const client = await this.getClient(input.subscriptionId);
+        await client.virtualMachines.beginDeleteAndWait(input.resourceGroupName, input.vmName);
+        return { status: 'deleted', vmName: input.vmName };
+    }
+}
+
+export default DeleteVirtualMachineTool;
